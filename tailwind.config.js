f98da0b95920ef38d17ef42ce932ddb9c14e91b8
R.Ferrarezzi <canalferrarezzi@gmail.com>
2022-03-22 23:34:54 -0300

    End project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: '#c7d0d8',
+        jungle: '#1a1d28',
+        gunmetal: '#2e3142',
+        green: '#43bc70',
+        red: '#f25f5c',
+      },
+      textColor: {
+        primary: '#ffffff',
+        secondary: '#c7d0d8',
+      },
+    },
   },
   plugins: [],
 }
